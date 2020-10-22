38f375aaac474eca058a1d86b40783b48ce7aee1
samfubuki <pbhardwaj.preet@gmail.com>
2020-10-22 19:50:02 +0530

    Updates

---
+++ precache-manifest.b6eaeeab3d6d1aa601e70d5763d91450.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04f60fea3fad71522f108a2405653e8c",
+    "url": "/VideoPlayerReact/index.html"
+  },
+  {
+    "revision": "b56471cd5eead68ee524",
+    "url": "/VideoPlayerReact/static/css/main.0d4e66d7.chunk.css"
+  },
+  {
+    "revision": "b0c90db6ac5c2b6510b1",
+    "url": "/VideoPlayerReact/static/js/2.c5c03262.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/VideoPlayerReact/static/js/2.c5c03262.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b56471cd5eead68ee524",
+    "url": "/VideoPlayerReact/static/js/main.7e142703.chunk.js"
+  },
+  {
+    "revision": "4fefb102588fb2e22a26",
+    "url": "/VideoPlayerReact/static/js/runtime-main.f429df42.js"
+  }
+]);
